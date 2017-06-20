c72b42a6e373af9c3173ae98c8ff0c7cc945f027
Thế Anh Nguyễn <NguyenTheAnh@thes-macbook-pro.local>
2017-06-20 12:26:44 +0700

    Initial Commit

---
+++ HanoiTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HanoiTourist
+//
+//  Created by Thế Anh Nguyễn on 6/20/17.
+//  Copyright © 2017 Thế Anh Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
